fc74aeafffd506a1bd77bd562439b55d1553c46c
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-02-29 23:28:47 +0100

    Added list of words or function to compute hours/minutes from 'one' to 'fiftynine'. Also a function check_minute and check_hour to see if the given text is in the list so we can check a string that we leave open in a RE on it's validity and value.

--- parse_dates.py
+++ parse_dates.py
@@ -1,8 +1,60 @@
 import datetime
+import re
+
+DIGITS = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+HOURS = DIGITS + [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty",
+    "twentyone",
+    "twentytwo",
+    "twentythree",
+    "twentyfour",
+]
+
+TENS = ["twenty", "thirty", "fourty", "fifty"]
+
+
+def check_hours(text):
+    if text in HOURS:
+        return True
+    return False
+
+
+def check_minutes(text):
+    extended_digits = [""] + DIGITS
+    minute_numbers = HOURS[:-5] + [
+        i + j for i in TENS for j in extended_digits
+    ]
+    if text in minute_numbers:
+        return True
+    return False
 
 
 def parse_time(description):
     dimension_case = "t"
+    check_minutes(description)
+    if re.search("o'clock", description):
+        return f"o'clock found"
     if dimension_case == "t":
         return datetime.time(1, 0)
     elif dimension_case == "d":
@@ -10,4 +62,4 @@ def parse_time(description):
 
 
 if __name__ == "__main__":
-    print(parse_time("Quarter to four"))
+    print(parse_time("four o'clock"))
