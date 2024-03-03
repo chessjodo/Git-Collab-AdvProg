cda6e36c3671e551006be31e12bf32a6b103ea72
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-03-03 21:48:17 +0100

    Add function for 'past' and 'oclock'. Changed fraction converter to include 'to' and 'past'. Only case where it still doesn't work is 'half past one', even though RE run on its own manages to recognize it

--- parse_dates.py
+++ parse_dates.py
@@ -33,6 +33,12 @@ HOURS = DIGITS + [
 
 TENS = ["twenty", "thirty", "fourty", "fifty"]
 
+EXTENDED_DIGITS = [
+    ""
+] + DIGITS  # add empty item to list to enable numbers like 'twenty'
+
+MINUTES = HOURS[:-5] + [i + j for i in TENS for j in EXTENDED_DIGITS]
+
 
 # check if a given text is a word representing an hour
 def check_hours(text):
@@ -43,32 +49,38 @@ def check_hours(text):
 
 # check if a given text is a word representing a minute measure
 def check_minutes(text):
-    extended_digits = [
-        ""
-    ] + DIGITS  # add empty item to list to enable numbers like 'twenty'
-    minute_numbers = HOURS[:-5] + [
-        i + j for i in TENS for j in extended_digits
-    ]  # compine words to form all numbers from one to fiftynine
-    if text in minute_numbers:
+    # compine words to form all numbers from one to fiftynine
+    if text in MINUTES:
         return True
     return False
 
 
 def check_to(description):
-    extended_digits = [""] + DIGITS
-    minute_numbers = HOURS[:-5] + [
-        i + j for i in TENS for j in extended_digits
-    ]
     re_to = r"\b(?:{})\s+to\s+\b(?:{})".format(
-        "|".join(minute_numbers), "|".join(HOURS)
+        "|".join(MINUTES), "|".join(HOURS)
     )  # RE to check whether an text is of the structure "<minute> to <hour>"
 
     # recognizing to expression
     if match_object := re.search(re_to, description):
         sub_string = description[match_object.start() : match_object.end()]
         split_string = sub_string.split(" to ")
-        minutes = 59 - minute_numbers.index(split_string[0])
+        minutes = 59 - MINUTES.index(split_string[0])
         hours = HOURS.index(split_string[1])
+        return datetime.time(hours, minutes)
+    return False
+
+
+def check_past(description):
+    re_past = r"\b(?:{})\s+past\s+\b(?:{})".format(
+        "|".join(MINUTES), "|".join(HOURS)
+    )  # RE to check whether an text is of the structure "<minute> past <hour>"
+
+    # recognizing past expression
+    if match_object := re.search(re_past, description):
+        sub_string = description[match_object.start() : match_object.end()]
+        split_string = sub_string.split(" past ")
+        minutes = MINUTES.index(split_string[0]) + 1
+        hours = HOURS.index(split_string[1]) + 1
         return datetime.time(hours, minutes)
     return False
 
@@ -77,17 +89,32 @@ def check_to(description):
 def convert_fractions(description):
     common_fractions = ["quarter", "half"]
     values = ["fifteen", "thirty"]
-    re_fractions = r"\b(a )?({})".format("|".join(common_fractions))
+    re_fractions = r"\b(a\s)?({})\s((to\s)?|(past\s)?)".format(
+        "|".join(common_fractions)
+    )
     adjusted_description = description
-    if matches := re.finditer(re_fractions, description):
+    matches = list(re.finditer(re_fractions, description))
+    if any(matches):
         for matched in matches:
+            addition = ""
+            if matched.group(5):
+                addition = " past "
+            else:
+                addition = " to "
             adjusted_description = (
                 adjusted_description[: matched.start()]
                 + values[common_fractions.index(matched.group(2))]
+                + addition
                 + adjusted_description[matched.end() :]
             )
-        print("NEW DESCRIPTION", adjusted_description)
     return adjusted_description
+
+
+# function that checks for "<hour> o'clock" and returns a datetime.time object
+def check_oclock(description):
+    re_oclock = r"\b({})\s+o'?clock".format("|".join(HOURS))
+    if matched := re.search(re_oclock, description):
+        return datetime.time(HOURS.index(matched.group(1)) + 1, 0)
 
 
 # main checker function
@@ -100,7 +127,10 @@ def parse_time(description):
 
     if check_to_result := check_to(description):
         return check_to_result  # datetime.time object
-
+    if check_past_result := check_past(description):
+        return check_past_result  # datetime.time object
+    if check_oclock_result := check_oclock(description):
+        return check_oclock_result  # datetime.time object
     # purely for testing
     if re.search("o'clock", description):
         return f"o'clock found"
